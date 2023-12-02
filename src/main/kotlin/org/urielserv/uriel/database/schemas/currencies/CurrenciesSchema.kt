ef033692c8578f91a7bf2bef2e2edac1cac4a866
dap <2367-dap@users.noreply.git.krews.org>
2023-12-02 12:13:55 +0100

    Prepare a currency refactor to make adding new currencies easier

---
+++ src/main/kotlin/org/urielserv/uriel/database/schemas/currencies/CurrenciesSchema.kt
@@ -0,0 +1,6 @@
+package org.urielserv.uriel.database.schemas.currencies
+
+import org.ktorm.schema.Table
+
+object CurrenciesSchema : Table<Nothing>("currencies") {
+}
---
+++ src/main/kotlin/org/urielserv/uriel/database/schemas/users/UserCurrenciesSchema.kt
@@ -0,0 +1,9 @@
+package org.urielserv.uriel.database.schemas.users
+
+import org.ktorm.schema.Table
+
+object UserCurrenciesSchema : Table<Nothing>("user_currencies") {
+
+
+
+}
