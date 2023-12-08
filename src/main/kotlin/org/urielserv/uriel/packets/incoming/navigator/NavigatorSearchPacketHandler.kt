c54534870d878803e8a48900d221f8b9eeabf2e8
notdap <90627349+notdap@users.noreply.github.com>
2023-12-08 15:08:11 +0100

    Temporarily log NavigatorSearchPacketHandler.kt

--- src/main/kotlin/org/urielserv/uriel/packets/incoming/navigator/NavigatorSearchPacketHandler.kt
+++ src/main/kotlin/org/urielserv/uriel/packets/incoming/navigator/NavigatorSearchPacketHandler.kt
@@ -1,5 +1,6 @@
 package org.urielserv.uriel.packets.incoming.navigator
 
+import io.klogging.logger
 import org.urielserv.uriel.extensions.readString
 import org.urielserv.uriel.networking.UrielServerClient
 import org.urielserv.uriel.packets.incoming.PacketHandler
@@ -7,11 +8,15 @@ import java.io.ByteArrayInputStream
 
 class NavigatorSearchPacketHandler : PacketHandler {
 
+    private val logger = logger(NavigatorSearchPacketHandler::class)
+
     override suspend fun handle(client: UrielServerClient, packet: ByteArrayInputStream) {
         if (client.habbo == null) return
 
         var searchCode = packet.readString()
         val data = packet.readString()
+
+        logger.info("Search code: $searchCode | Data: $data")
 
         if (searchCode == "query" || searchCode == "groups") {
             searchCode = "hotel_view"
