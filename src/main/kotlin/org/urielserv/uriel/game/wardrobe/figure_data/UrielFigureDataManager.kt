dac4c2e5ca5fcac44a70340ddf2c8fdc2910a460
notdap <90627349+notdap@users.noreply.github.com>
2024-01-06 10:40:36 +0100

    Allow loading a local file for the FigureData.json and update the comments in uriel_hotel_settings.toml

--- src/main/kotlin/org/urielserv/uriel/game/wardrobe/figure_data/UrielFigureDataManager.kt
+++ src/main/kotlin/org/urielserv/uriel/game/wardrobe/figure_data/UrielFigureDataManager.kt
@@ -1,7 +1,9 @@
 package org.urielserv.uriel.game.wardrobe.figure_data
 
 import kotlinx.serialization.json.Json
+import java.io.File
 import java.net.URI
+import java.net.URL
 
 class UrielFigureDataManager(url: String) {
 
@@ -13,12 +15,36 @@ class UrielFigureDataManager(url: String) {
     }
 
     init {
-        val jsonContents = URI(url).toURL().openStream().readAllBytes().toString(Charsets.UTF_8)
+        val jsonContents = readContents(url)
 
         val figureData = json.decodeFromString<FigureData>(jsonContents)
 
         palettes = figureData.palettes
         setTypes = figureData.setTypes
+    }
+
+    fun readContents(url: String): String {
+        return if (isLocalFile(url)) {
+            readLocalFile(url)
+        } else {
+            readWebFile(url)
+        }
+    }
+
+    private fun isLocalFile(url: String): Boolean {
+        return try {
+            File(url).isFile
+        } catch (e: Exception) {
+            false
+        }
+    }
+
+    private fun readLocalFile(filePath: String): String {
+        return File(filePath).readText()
+    }
+
+    private fun readWebFile(url: String): String {
+        return URI(url).toURL().readText()
     }
 
 }
