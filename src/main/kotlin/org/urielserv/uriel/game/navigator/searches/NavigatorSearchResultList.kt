5ed582f93a604182f05f63d3057524d021513f99
notdap <90627349+notdap@users.noreply.github.com>
2024-01-10 18:26:32 +0100

    Remove Room#appendToPacket

--- src/main/kotlin/org/urielserv/uriel/game/navigator/searches/NavigatorSearchResultList.kt
+++ src/main/kotlin/org/urielserv/uriel/game/navigator/searches/NavigatorSearchResultList.kt
@@ -28,7 +28,61 @@ class NavigatorSearchResultList(
 
         packet.appendInt(rooms.size)
         for (room in rooms) {
-            room.appendToPacket(packet)
+            packet.appendInt(room.info.id)
+            packet.appendString(room.info.name)
+
+            if (room.info.isPublic) {
+                packet.appendInt(0)
+                packet.appendString("")
+            } else {
+                packet.appendInt(room.info.ownerHabboInfo.id)
+                packet.appendString(room.info.ownerHabboInfo.username)
+            }
+
+            packet.appendInt(room.info.accessType.ordinal)
+
+            packet.appendInt(room.info.users)
+            packet.appendInt(room.info.maximumUsers)
+
+            packet.appendString(room.info.description)
+
+            packet.appendInt(0)
+            packet.appendInt(room.info.score)
+
+            packet.appendInt(0)
+            packet.appendInt(room.info.flatCategory.id)
+
+            val tags = room.info.tags.split(";")
+            packet.appendInt(tags.size)
+            for (tag in tags) {
+                packet.appendString(tag)
+            }
+
+            var base = 0
+
+            /*
+            TODO: Add Guild to base
+            if (info.guildId > 0) {
+                base = base or 2
+            }
+             */
+
+            /*
+            TODO: Add support for Promotions
+            if (info.isPromoted) {
+                base = base or 4
+            }
+             */
+
+            if (!room.info.isPublic) {
+                base = base or 8
+            }
+
+            packet.appendInt(base)
+
+            // TODO: Add Guild to packet
+
+            // TODO: Add Promotion Info to packet
         }
     }
 
